9c567bbe1082ca89df3f2187cafe5004faf06bf8
czhizhpon <czhizhpon@est.ups.edu.ec>
2020-12-03 18:08:59 -0500

    Entidades BillHead, BillDetail y Products

--- src/ec/edu/ups/entities/BillDetail.java
+++ src/ec/edu/ups/entities/BillDetail.java
@@ -8,21 +8,36 @@ import javax.persistence.*;
  *
  */
 @Entity
-@Table(name="BILL_DETAILS")
-
+@Table(name = "BILL_DETAILS")
 public class BillDetail implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "det_id")
 	private int detId;
+	
+	@Column(name = "det_amount")
 	private int detAmount;
+	
+	@Column(name = "det_unit_price", precision = 12, scale = 2)
 	private double detUnitPrice;
+	
+	@Column(name = "det_total", precision = 12, scale = 2)
 	private double detTotal;
+	
+	@Column(name = "det_deleted", columnDefinition = "BOOLEAN DEFAULT 0")
 	private boolean detDeleted;
 
+	@ManyToOne
+	@JoinColumn
+	private BillHead detBillHead;
+	
+	@OneToOne(cascade = CascadeType.ALL, mappedBy = "proBillDetail")
+	private Product detProduct;
+	
 	public BillDetail() {
 		super();
 	}
-   
 }
--- src/ec/edu/ups/entities/Category.java
+++ src/ec/edu/ups/entities/Category.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
  *
  */
 @Entity
-@Table(name="CATEGORIES")
+@Table(name = "CATEGORIES")
 
 public class Category implements Serializable {
 
